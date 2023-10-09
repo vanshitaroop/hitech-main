f0cac471598f42c0e31050da9cec98d66c1096e3
vanshita Roopchandani <81114486+vanshitaroop@users.noreply.github.com>
2023-10-09 15:45:08 +0530

    latest commit

---
+++ src/components/header/header1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header1 = () => {
+  return (
+    <div className="navbar-brand">
+      <a className="navbar-item logo" href="\" style={{marginLeft: "10%"}}>
+        <img src="Sources/images/hitech_logo.png" alt="logo" />
+      </a>
+    </div>
+  );
+}
+
+export default Header1
